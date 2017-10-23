7522dda17e3fac6e9e0ee41473619a1d8ee7145c
Ryan Duplooy <duplooyra@s.dcsdk12.org>
2017-10-23 08:07:07 -0600

    Print names with scores

--- iteration.py
+++ iteration.py
@@ -10,3 +10,6 @@ def iterate (list):
     
     #for i in range(0,len(list)):
         #print list[i]
+def print_scores(names, scores):
+    for i in range(0, len(names)):
+        print names[i] , " scored " , scores[i]
