d3f81926037379f2b96a976fe00b98f47230816f
duplooyr <007Tropix@gmail.com>
2017-10-23 19:27:04 -0600

    Created Assignment

---
+++ Assignment.py
@@ -0,0 +1,9 @@
+number = int(raw_input("Enter your number here for it to be multiplied by 2 and then added to 10 "))
+
+number_multiplied = number * 2
+
+final = number_multiplied + 10
+
+print "The result is %d" % final
+
+#This was done at home.
